1f1c2490d3bfe471e28eda5940cdc4729623790e
ndsrf <jgarciamagna@gmail.com>
2015-05-12 09:56:30 +0100

    Lost source code so had to decompile - Added support for uninstallation of components and file patterns

--- src/Comparisons.cs
+++ src/Comparisons.cs
@@ -1,66 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// Decompiled with JetBrains decompiler
+
+using System;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FolderToGacIfDifferent
 {
-    class Comparisons
+    internal class Comparisons
     {
         public static bool FilesContentsAreEqual(FileInfo fileInfo1, FileInfo fileInfo2)
         {
-            bool result;
-
             if (fileInfo1.Length != fileInfo2.Length)
+                return false;
+            using (FileStream fileStream1 = fileInfo1.OpenRead())
             {
-                result = false;
+                using (FileStream fileStream2 = fileInfo2.OpenRead())
+                    return Comparisons.StreamsContentsAreEqual((Stream)fileStream1, (Stream)fileStream2);
             }
-            else
-            {
-                using (var file1 = fileInfo1.OpenRead())
-                {
-                    using (var file2 = fileInfo2.OpenRead())
-                    {
-                        result = StreamsContentsAreEqual(file1, file2);
-                    }
-                }
-            }
-
-            return result;
         }
 
         private static bool StreamsContentsAreEqual(Stream stream1, Stream stream2)
         {
-            const int bufferSize = 2048 * 2;
-            var buffer1 = new byte[bufferSize];
-            var buffer2 = new byte[bufferSize];
-
-            while (true)
+            byte[] buffer1 = new byte[4096];
+            byte[] buffer2 = new byte[4096];
+        label_1:
+            int num1 = stream1.Read(buffer1, 0, 4096);
+            int num2 = stream2.Read(buffer2, 0, 4096);
+            if (num1 != num2)
+                return false;
+            if (num1 == 0)
+                return true;
+            int num3 = (int)Math.Ceiling((double)num1 / 8.0);
+            for (int index = 0; index < num3; ++index)
             {
-                int count1 = stream1.Read(buffer1, 0, bufferSize);
-                int count2 = stream2.Read(buffer2, 0, bufferSize);
-
-                if (count1 != count2)
-                {
+                if (BitConverter.ToInt64(buffer1, index * 8) != BitConverter.ToInt64(buffer2, index * 8))
                     return false;
-                }
-
-                if (count1 == 0)
-                {
-                    return true;
-                }
-
-                int iterations = (int)Math.Ceiling((double)count1 / sizeof(Int64));
-                for (int i = 0; i < iterations; i++)
-                {
-                    if (BitConverter.ToInt64(buffer1, i * sizeof(Int64)) != BitConverter.ToInt64(buffer2, i * sizeof(Int64)))
-                    {
-                        return false;
-                    }
-                }
             }
+            goto label_1;
         }
     }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -1,128 +1,138 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.EnterpriseServices.Internal;
 using System.IO;
 using System.Linq;
 using System.Security.Principal;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FolderToGacIfDifferent
 {
-    class Program
+    internal class Program
     {
-        private const string EXTDLL = ".dll";
+        private const int EXTENSIONLENGHT = 4; // ".exe" or ".dll"
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             try
             {
+                bool uninstallMode = false;
+                string[] searchPatterns;
+
                 if (args.Length == 0)
                 {
-                    Console.WriteLine("Usage: " + System.AppDomain.CurrentDomain.FriendlyName + " <DLL_PATH>");
-                    Console.WriteLine("Example: " + System.AppDomain.CurrentDomain.FriendlyName + " c:\\gac");
-                    return;
-                }
+                    Console.WriteLine("Usage: " + AppDomain.CurrentDomain.FriendlyName + "<DLL_PATH>  [/u] [Comma separated patterns including extensions]");
+                    Console.WriteLine("Example: " + AppDomain.CurrentDomain.FriendlyName + " c:\\gac");
+                    Console.WriteLine("Example: " + AppDomain.CurrentDomain.FriendlyName + " c:\\gac /u Sequel*.dll,Sequel*.exe");
+                    Console.WriteLine("By default it will install all the *.exe and *.dll files from the specified folder.");
 
-                if (!Program.IsUserAdministrator())
+                }
+                else if (!Program.IsUserAdministrator())
                 {
                     Console.WriteLine("This application must be run as an Administrator.");
-                    return;
                 }
-
-                System.EnterpriseServices.Internal.Publish p = new System.EnterpriseServices.Internal.Publish();
-
-                DirectoryInfo inputFolder = new DirectoryInfo(args[0]);
-
-                Console.WriteLine("Working folder " + inputFolder.FullName);
-
-                foreach (FileInfo fileInput in inputFolder.EnumerateFiles("*" + EXTDLL))
+                else
                 {
-                    bool fileFound = false;
-
-                    List<FileInfo> fileDestinationList = SearchForFileInGAC(fileInput);
-
-                    foreach (FileInfo fileDestination in fileDestinationList)
+                    if (args.Length > 1)
                     {
-                        if (Comparisons.FilesContentsAreEqual(fileInput, fileDestination))
+                        uninstallMode = "/u".Equals(args[1], StringComparison.CurrentCultureIgnoreCase);
+                    }
+
+                    Publish publish = new Publish();
+                    DirectoryInfo directoryInfo = new DirectoryInfo(args[0]);
+                    Console.WriteLine("Working folder " + directoryInfo.FullName);
+                    if (args.Length > 2)
+                    {
+                        searchPatterns = args[2].Split(',');
+                    }
+                    else
+                    {
+                        searchPatterns = new string[] { "*.dll", "*.exe" };
+                    }
+                    var fileList = MyDirectory.GetFiles(directoryInfo.FullName, searchPatterns, System.IO.SearchOption.TopDirectoryOnly);
+                    foreach (string fileName in fileList)
+                    {
+                        FileInfo fileInfo = new FileInfo(fileName);
+
+                        if (uninstallMode)
                         {
-                            fileFound = true;
-                            break;
+                            Console.WriteLine("Removing " + (object)fileInfo + " from the GAC.");
+                            publish.GacRemove(fileInfo.FullName);
+                        }
+                        else
+                        {
+                            bool flag = false;
+                            List<FileInfo> list = Program.SearchForFileInGAC(fileInfo);
+                            foreach (FileInfo fileInfo2 in list)
+                            {
+                                if (Comparisons.FilesContentsAreEqual(fileInfo, fileInfo2))
+                                {
+                                    flag = true;
+                                    break;
+                                }
+                            }
+                            if (!flag && Enumerable.Count<FileInfo>((IEnumerable<FileInfo>)list) > 0)
+                            {
+                                Console.WriteLine("Removing " + (object)fileInfo + " from the GAC.");
+                                publish.GacRemove(fileInfo.FullName);
+                            }
+                            if (!flag)
+                            {
+                                Console.WriteLine("Adding " + (object)fileInfo + " to the GAC.");
+                                publish.GacInstall(fileInfo.FullName);
+                            }
                         }
                     }
-
-                    if (!fileFound && fileDestinationList.Count() > 0)
-                    {
-                        // We need to remove the file from the gac
-                        Console.WriteLine("Removing " + fileInput + " from the GAC.");
-                        p.GacRemove(fileInput.Name.Substring(0, fileInput.Name.Length - EXTDLL.Length));
-                    }
-
-                    if (!fileFound)
-                    {
-                        // We neeed to add the file to the gac
-                        Console.WriteLine("Adding " + fileInput + " to the GAC.");
-                        p.GacInstall(fileInput.FullName);
-                    }
                 }
             }
-            catch (Exception e)
+            catch (Exception ex)
             {
-                Console.WriteLine("Ouch!... " + e.Message);
-                Console.WriteLine(e.StackTrace.ToString());
+                Console.WriteLine("Ouch!... " + ex.Message);
+                Console.WriteLine(ex.StackTrace.ToString());
             }
-
         }
 
         private static List<FileInfo> SearchForFileInGAC(FileInfo fileInput)
         {
-            List<FileInfo> fileInGac = new List<FileInfo>();
-            string[] strGACFolders = ConfigurationManager.AppSettings["GACFOLDERS_COMMASEPARATED"].Split(new char[] { ',' });
-
-            string fileInputNameWithoutExtension = fileInput.Name.Substring(0, fileInput.Name.Length - EXTDLL.Length);
-
-            foreach(string oneGacFolder in strGACFolders)
+            List<FileInfo> list = new List<FileInfo>();
+            string[] strArray;
+            try
             {
-                DirectoryInfo directory = new DirectoryInfo(oneGacFolder + '\\' + fileInputNameWithoutExtension);
-                if (directory.Exists)
+                strArray = ConfigurationManager.AppSettings["GACFOLDERS_COMMASEPARATED"].Split(',');
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Cannot read from config file the GACFOLDERS_COMMASEPARATED setting...", ex);
+            }
+            string str = fileInput.Name.Substring(0, fileInput.Name.Length - EXTENSIONLENGHT);
+            foreach (object obj in strArray)
+            {
+                DirectoryInfo directoryInfo1 = new DirectoryInfo((string)obj + (object)'\\' + str);
+                if (directoryInfo1.Exists)
                 {
-                    foreach (DirectoryInfo folderPerVersion in directory.EnumerateDirectories())
+                    foreach (DirectoryInfo directoryInfo2 in directoryInfo1.EnumerateDirectories())
                     {
-                        var listTemp = folderPerVersion.EnumerateFiles(fileInput.Name);
-                        if (listTemp.Count() == 1)
-                        {
-                            fileInGac.Add(listTemp.First());
-                        }
-                        else if (listTemp.Count() > 1)
-                        {
-                            throw new ApplicationException("More than 1 file in this folder: " + folderPerVersion);
-                        }
+                        IEnumerable<FileInfo> source = directoryInfo2.EnumerateFiles(fileInput.Name);
+                        if (Enumerable.Count<FileInfo>(source) == 1)
+                            list.Add(Enumerable.First<FileInfo>(source));
+                        else if (Enumerable.Count<FileInfo>(source) > 1)
+                            throw new ApplicationException("More than 1 file in this folder: " + (object)directoryInfo2);
                     }
                 }
             }
-            return fileInGac;
+            return list;
         }
-        
+
         public static bool IsUserAdministrator()
         {
-            //bool value to hold our return value
-            bool isAdmin;
             try
             {
-                //get the currently logged in user
-                WindowsIdentity user = WindowsIdentity.GetCurrent();
-                WindowsPrincipal principal = new WindowsPrincipal(user);
-                isAdmin = principal.IsInRole(WindowsBuiltInRole.Administrator);
+                return new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator);
             }
             catch (UnauthorizedAccessException)
             {
-                isAdmin = false;
+                return false;
             }
-            catch (Exception)
-            {
-                isAdmin = false;
-            }
-            return isAdmin;
         }
     }
 }
